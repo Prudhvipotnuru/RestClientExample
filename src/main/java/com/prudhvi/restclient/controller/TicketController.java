0292de330162f244de26d9275301317fb0006075
prudhvipotnuru <prudhvipotnuru@gmail.com>
2026-02-28 13:03:08 +0530

    Did Asynchronous call with WebClient Interface

--- src/main/java/com/prudhvi/restclient/controller/TicketController.java
+++ src/main/java/com/prudhvi/restclient/controller/TicketController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.prudhvi.restclient.model.Passenger;
@@ -35,5 +36,17 @@ public class TicketController {
 	public String demo(Model model) {
 		model.addAttribute("passenger", new Passenger());
 		return "booking";
+	}
+	
+	@GetMapping("/getTicket")
+	@ResponseBody
+	public Ticket getTicket(@RequestParam Integer ticketId) {
+		return ticketService.ticketInfoSync(ticketId);
+	}
+	
+	@GetMapping("/getTicket2")
+	@ResponseBody
+	public String getTicketAsync(@RequestParam Integer ticketId) {
+		return ticketService.ticketInfoASync(ticketId);
 	}
 }
--- src/main/java/com/prudhvi/restclient/service/TicketService.java
+++ src/main/java/com/prudhvi/restclient/service/TicketService.java
@@ -1,5 +1,6 @@
 package com.prudhvi.restclient.service;
 
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,8 @@ public class TicketService {
 	private String PASSENGER_URL;
 	@Value("${rest_client.bookticket.url}")
 	private String BOOKTICKET_URL;
+	@Value("${rest_client.getticket.url}")
+	private String GETTICKET_URL;
 
 	public Passenger passenger() {
 		WebClient webClient = WebClient.create();
@@ -38,6 +41,36 @@ public class TicketService {
 		.bodyToMono(Ticket.class)
 		.block();
 		return ticket;
+	}
+	
+	public Ticket ticketInfoSync(Integer ticketId) {
+		WebClient webClient = WebClient.create();
+		System.out.println("sync method started....");
+		Ticket ticket = webClient.get()
+				.uri(GETTICKET_URL, ticketId)
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Ticket.class).block();
+		System.out.println(ticket);
+		System.out.println("sync method ended....");
+		return ticket;
+	}
+	
+	public String ticketInfoASync(Integer ticketId) {
+		WebClient webClient = WebClient.create();
+		System.out.println("async method started....");
+		webClient.get()
+				.uri(GETTICKET_URL, ticketId)
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.bodyToMono(Ticket.class)
+				.subscribe(tick->printTicket(tick));
+		System.out.println("async method ended....");
+		return "Go to console";
+	}
+	
+	public void printTicket(Ticket ticket) {
+		System.out.println(ticket);
 	}
 
 }
